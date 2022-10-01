6dc915a2a03fad66e6b2cbdf0ed8bf4c7f3f4ee1
tianjingli <569302975@qq.com>
2022-10-01 15:45:10 +0800

    remove md_lib js.

--- js/aes.js
+++ js/aes.js
@@ -3,7 +3,7 @@
 
 const mode = {
     // iv: CryptoJS.enc.Utf8.parse("ABCDEF1234123412"), // 十六位十六进制数作为密钥偏移量
-    iv: CryptoJS.enc.Utf8.parse(hex_md5_16B("tjlaes2022")), // 密钥偏移量
+    iv: CryptoJS.enc.Utf8.parse((""+CryptoJS.MD5("tjlaes2022")).slice(8, 24)), // 密钥偏移量
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
 }
@@ -12,14 +12,14 @@ const mode = {
 
 // 加密方法
 function aesStringEncrypt(key, data) {
-    key = CryptoJS.enc.Utf8.parse(hex_md5(key));
+    key = CryptoJS.enc.Utf8.parse(CryptoJS.MD5(key));
     let srcs = CryptoJS.enc.Utf8.parse(data);
     let encrypted = CryptoJS.AES.encrypt(srcs, key, mode);
     return CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
 }
 // 解密方法
 function aesStringDecrypt(key, data) {
-    key = CryptoJS.enc.Utf8.parse(hex_md5(key));
+    key = CryptoJS.enc.Utf8.parse(CryptoJS.MD5(key));
     let encryptedHexStr = CryptoJS.enc.Base64.parse(data);
     let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
     let decrypt = CryptoJS.AES.decrypt(srcs, key, mode);
@@ -32,7 +32,7 @@ function aesStringDecrypt(key, data) {
 function aesFileEncrypt(key, data) {
     // data为ArrayBuffer类型的数据
     data = arrayBufferToWordArray(data);
-    key = CryptoJS.enc.Hex.parse(hex_md5(key));
+    key = CryptoJS.enc.Hex.parse(CryptoJS.MD5(key));
     let encrypted = CryptoJS.AES.encrypt(data, key, mode);
     return wordArrayToArrayBuffer(encrypted.ciphertext);
 }
@@ -40,7 +40,7 @@ function aesFileEncrypt(key, data) {
 function aesFileDecrypt(key, data) {
     // data为ArrayBuffer类型的数据
     data = arrayBufferToWordArray(data);
-    key = CryptoJS.enc.Hex.parse(hex_md5(key));
+    key = CryptoJS.enc.Hex.parse(CryptoJS.MD5(key));
     let decrypt = CryptoJS.AES.decrypt({ ciphertext: data }, key, mode);
     return wordArrayToArrayBuffer(decrypt);
 }
